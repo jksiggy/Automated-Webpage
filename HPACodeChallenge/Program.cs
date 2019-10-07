be894e2d36aa420fa710f307c5f58cf2941cac94
joab sigrah <jksiggy@gmail.com>
2019-10-06 20:52:03 -0500

    first commit

---
+++ HPACodeChallenge/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Diagnostics;
+
+namespace HPACodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tst = new ProcessStartInfo(@"C:\Program Files\internet explorer\iexplore.exe");
+            tst.Arguments = "http://hpadevtest.azurewebsites.net/";
+            Process.Start(tst);
+        }
+    }
+}
